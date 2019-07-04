364a6b0607bb503a43314db27402813a8e4fc2fd
Luke Schreur <luke.schreur@gmail.com>
2019-07-04 16:24:29 +1000

    tweaks.

---
+++ src/container/index.ts
@@ -0,0 +1,30 @@
+
+// import Container from '@northern/di'
+
+// // import logger from '../app/Util/logger'
+// // import initPersistanceService from './service/persistance'
+// // import initQueueService from './service/queue'
+// // import initTemplateService from './service/template'
+// // import initMessageService from './service/message'
+// // import initTransmissionService from './service/transmission'
+// // import initIntegrationService from './service/integration'
+// // import initCommand from './command'
+// // import initQuery from './query'
+
+// export default async (config: any) => {
+//   const container = new Container()
+
+//   // container.service('logger', () => logger)
+//   // container.service('config', () => config)
+
+//   // await initPersistanceService(container)
+//   // await initQueueService(container)
+//   // await initTemplateService(container)
+//   // await initMessageService(container)
+//   // await initTransmissionService(container)
+//   // await initIntegrationService(container)
+//   // await initCommand(container)
+//   // await initQuery(container)
+
+//   return container
+// }
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@
 import Koa from 'koa'
 import morgan from 'morgan'
 
+// import container from './container'
+
 const init = async() => {
   const server = new Koa();
+
+  // server.context.container = await container({}/*config()*/)
 
   server.use(async (ctx: any) => {
     ctx.body = 'Hello World';
