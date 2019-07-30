d0ede451029bfaa3258345e2d7871ae726339a4f
Luke Schreur <luke.schreur@gmail.com>
2019-07-30 12:04:45 +1000

    add container comments.

--- src/util/Container.ts
+++ src/util/Container.ts
@@ -1,12 +1,23 @@
 
+/**
+ * A simple service locator, a.k.a dependency injection container (DIC).
+ */
 export default class Container {
-  services: Map<any, any>
+  services: Map<string, any>
 
   constructor() {
     this.services = new Map()
   }
 
-  service(name: string, callback: Function, lazy: boolean = false) {
+  /**
+   * Register a new service. Use the "lazy" option to spawn the instance at
+   * request time rather than initialization time.
+   * 
+   * @param name The name of the service (must be unique)
+   * @param callback A function that returns the service instance
+   * @param lazy If the service is lazy loaded.
+   */
+  service(name: string, callback: Function, lazy: boolean = false): void {
     if (this.services.has(name)) {
       throw new Error(`Service with name '${name}' already exists.`)
     }
@@ -21,7 +32,13 @@ export default class Container {
     }
   }
 
-  factory(name: string, callback: Function) {
+  /**
+   * Register a factory. A factory always returns a new instance.
+   * 
+   * @param name The name of the factory (must be unique)
+   * @param callback A function that returns the factory instance
+   */
+  factory(name: string, callback: Function): void {
     if (this.services.has(name)) {
       throw new Error(`Service with name '${name}' already exists.`)
     }
@@ -29,7 +46,12 @@ export default class Container {
     this.services.set(name, callback)
   }
 
-  get(name: string) {
+  /**
+   * Returns an instance of the service or factory.
+   * 
+   * @param name The name of the service or factory to get the instance of.
+   */
+  get(name: string): any {
     if (!this.services.has(name)) {
       throw new Error(`Service with name '${name}' already exists.`)
     }
